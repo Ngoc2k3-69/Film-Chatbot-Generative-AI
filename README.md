600573b6c44b3697c61ecbb5bef667519fa76b17
ngoclv21 <ngoclv21@uef.edu.vn>
2024-12-27 17:08:42 +0700

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Film-Chatbot-Generative-AI
+
+
+# How to run?
+### STEPS:
+clone the repository
+''''bash
+Project: https://github.com/
+
+### STEP 01- Create a conda environment after opening the repository
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ="Generative AI Project",
+    version="0.0.0",
+    author_email="ngoclv826.work@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
